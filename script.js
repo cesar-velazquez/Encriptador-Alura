b0aee8455455ea4426a961bded98e5f165cc241f
César <velazquezortizcesar@gmail.com>
2023-01-30 17:27:02 -0600

    Se agrega una nueva actualización en el script.js

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ function copiar(){
 
 
 
-// La siguiente funcion valida el elemento input
+// Inicio de la función validar
 function validar() {
     // Variable que usaremos para determinar si el input es valido
     let isValid = false;
@@ -72,57 +72,54 @@ function validar() {
     // El input que queremos validar
     const input = document.forms['validationForm']['letras'];
 
-    //El div con el mensaje de advertencia:
+    //Mensaje de advertencia:
   
     const message = document.getElementById('message');
   
 
     input.willValidate = false;
 
-    // El tamaño maximo para nuestro input
+    // El tamaño maximo para nuestra text area
     const maximo = 300;
 
-    // El pattern que vamos a comprobar
+    // Expresión regular que solo acepta letras minúsculas y sin acentos ni caracteres especiales
     const pattern = new RegExp('^[ a-z ]+$', 'i');
 
-    // Primera validacion, si input esta vacio entonces no es valido
+    // Primera validacion, si mi textarea esta vacio entonces no es valido
     if(!input.value) {
       isValid = false;
     } else {
-      // Segunda validacion, si input es mayor que 35
+      // Segunda validacion, si input es mayor que 300
       if(input.value.length > maximo) {
         isValid = false;
       } else {
-        // Tercera validacion, si input contiene caracteres diferentes a los permitidos
+        // Tercera validacion, si el textarea contiene caracteres diferentes a los permitidos
         if(!pattern.test(input.value)){ 
-        // Si queremos agregar letras acentuadas y/o letra ñ debemos usar
-        // codigos de Unicode (ejemplo: Ñ: \u00D1  ñ: \u00F1)
           isValid = false;
         } else {
-          // Si pasamos todas la validaciones anteriores, entonces el input es valido
+          // Si pasamos todas la validaciones anteriores, entonces el textArea es valido
           isValid = true;
         }
       }
     }
 
-    //Ahora coloreamos el borde de nuestro input
+    //Colorear el contorno del textArea
     if(!isValid) {
-      // rojo: no es valido
-      input.style.borderColor = 'red'; // me parece que 'salmon' es un poco menos agresivo que 'red'
+      // rojo: ERROR
+      input.style.borderColor = 'red'; 
       // mostramos mensaje
       message.hidden = false;
     } else {
-      // verde: si es valido
-      input.style.borderColor = 'green'; // 'palegreen' se ve mejor que 'green' en mi opinion
+      // verde: Es valido
+      input.style.borderColor = 'green'; 
       // ocultamos mensaje;
       message.hidden = true;
     }
-
     // devolvemos el valor de isValid
     return isValid;
   }
 
-  // Por último, nuestra función que verifica si el campo es válido antes de realizar cualquier otra acción.
+  // Verificamos que todo este correcto
   function verificar() {
     const valido = validar();
     if (!valido) {                
